7e8d1f31bd8c340724697141aceb51dfb9a23d14
fredlc <fredlefevre@outlook.com>
2021-01-13 17:46:10 -0500

    Added MainTabBarController and UserProfileController

---
+++ Instagram/MainTabBarController.swift
@@ -0,0 +1,28 @@
+//
+//  MainTabBarController.swift
+//  Instagram
+//
+//  Created by Fred Lefevre on 2021-01-13.
+//  Copyright © 2021 Fred Lefevre. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let layout = UICollectionViewFlowLayout()
+        let userProfileController = UserProfileController(collectionViewLayout: layout)
+        
+        let navController = UINavigationController(rootViewController: userProfileController)
+        
+        navController.tabBarItem.image = #imageLiteral(resourceName: "profile_unselected")
+        navController.tabBarItem.selectedImage = #imageLiteral(resourceName: "profile_selected")
+        
+        tabBar.tintColor = .black
+        
+        viewControllers = [navController, UIViewController()]
+    }
+}
