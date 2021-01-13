9162c67a53d69841ec2969558635ce37e9143385
fredlc <fredlefevre@outlook.com>
2021-01-13 17:08:11 -0500

    Implemented image picker and profile image upload using Firebase Storage

--- Instagram/ViewController.swift
+++ Instagram/ViewController.swift
@@ -9,13 +9,37 @@
 import UIKit
 import Firebase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     let plusPhotoButton: UIButton = {
         let button = UIButton(type: .system)
         button.setImage(#imageLiteral(resourceName: "plus_photo").withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handlePlusPhoto), for: .touchUpInside)
         return button
     }()
+    
+    @objc func handlePlusPhoto() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let editedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            plusPhotoButton.setImage(editedImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        } else if let originalImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            plusPhotoButton.setImage(originalImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        }
+        
+        plusPhotoButton.layer.cornerRadius = plusPhotoButton.frame.width / 2
+        plusPhotoButton.layer.masksToBounds = true
+        plusPhotoButton.layer.borderColor = UIColor.black.cgColor
+        plusPhotoButton.layer.borderWidth = 3
+        
+        dismiss(animated: true, completion: nil)
+    }
     
     let emailTextField: UITextField = {
         let textField = UITextField()
@@ -89,6 +113,49 @@ class ViewController: UIViewController {
             }
             
             print("Successfully registered user: ", result?.user.uid ?? "")
+            
+            guard let image = self.plusPhotoButton.imageView?.image else { return }
+            
+            guard let uploadData = image.jpegData(compressionQuality: 0.3) else { return }
+            
+            let filename = NSUUID().uuidString
+                
+            let storageRef = Storage.storage().reference().child("profile_images").child(filename)
+            
+            storageRef.putData(uploadData, metadata: nil) { (metadata, err) in
+                
+                if let err = err {
+                    print("Failed to upload profile image: ", err)
+                    return
+                }
+                
+                storageRef.downloadURL { (downloadUrl, err) in
+                    
+                    if let err = err {
+                        print("Failed to download image url: ", err)
+                        return
+                    }
+                    
+                    guard let profileImageUrl = downloadUrl?.absoluteString else { return }
+                    
+                    print("Successfully uploaded profile image:", profileImageUrl)
+                    
+                    guard let uid = result?.user.uid else { return }
+
+                    let dictionaryValues = ["username": username, "profile_image": profileImageUrl]
+                    let values = [uid: dictionaryValues]
+
+                    Database.database().reference().child("users").updateChildValues(values) { (err, ref) in
+                        if let err = err {
+                            print("Failed to save user to database: ", err)
+                            return
+                        }
+
+                        print("Successfully saved user to database.")
+                    }
+                }
+                
+            }
         }
     }
 
