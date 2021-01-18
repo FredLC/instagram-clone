ceb492c07e0eb4ab4044420b9996c095e2dac335
fredlc <fredlefevre@outlook.com>
2021-01-18 17:26:43 -0500

    Implemented photo selection

---
+++ Instagram/AddPhoto/PhotoSelectorHeader.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoSelectorHeader.swift
+//  Instagram
+//
+//  Created by Fred Lefevre on 2021-01-18.
+//  Copyright © 2021 Fred Lefevre. All rights reserved.
+//
+
+import UIKit
+
+class PhotoSelectorHeader: UICollectionViewCell {
+    let photoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.backgroundColor = .systemPink
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(photoImageView)
+        photoImageView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
