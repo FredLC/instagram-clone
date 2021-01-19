2267797cf307f1aeedb13876930112af7f5ed108
fredlc <fredlefevre@outlook.com>
2021-01-19 16:32:57 -0500

    Built UserSearchController and UserSearchCell

---
+++ Instagram/Search/UserSearchController.swift
@@ -0,0 +1,51 @@
+//
+//  UserSearchController.swift
+//  Instagram
+//
+//  Created by Fred Lefevre on 2021-01-19.
+//  Copyright © 2021 Fred Lefevre. All rights reserved.
+//
+
+import UIKit
+
+class UserSearchController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    let searchBar: UISearchBar = {
+        let sb = UISearchBar()
+        sb.placeholder = "Enter username"
+        UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).backgroundColor = UIColor.rgb(red: 240, green: 240, blue: 240)
+        return sb
+    }()
+    
+    let cellId = "cellId"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.backgroundColor = .white
+        
+        navigationController?.navigationBar.addSubview(searchBar)
+        
+        let navBar = navigationController?.navigationBar
+        
+        searchBar.anchor(top: navBar?.topAnchor, left: navBar?.leftAnchor, bottom: navBar?.bottomAnchor, right: navBar?.rightAnchor, paddingTop: 0, paddingLeft: 8, paddingBottom: 0, paddingRight: 8, width: 0, height: 0)
+        
+        collectionView.register(UserSearchCell.self, forCellWithReuseIdentifier: cellId)
+        
+        collectionView.alwaysBounceVertical = true
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 66)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        
+        return cell
+    }
+}
