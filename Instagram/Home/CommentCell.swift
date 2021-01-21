df34f8480f618bea46ea2610ed5002e09eea7279
fredlc <fredlefevre@outlook.com>
2021-01-20 19:47:00 -0500

    Implemented auto-resizing of comment cells

--- Instagram/Home/CommentCell.swift
+++ Instagram/Home/CommentCell.swift
@@ -12,24 +12,40 @@ class CommentCell: UICollectionViewCell {
     
     var comment: Comment? {
         didSet {
-            textLabel.text = comment?.text
+            guard let comment = comment else { return }
+            
+            let attributedText = NSMutableAttributedString(string: comment.user.username, attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 14)])
+            
+            attributedText.append(NSAttributedString(string: " \(comment.text)", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 14)]))
+            
+            textView.attributedText = attributedText
+            profileImageView.loadImage(urlString: comment.user.profileImageUrl)
         }
     }
     
-    let textLabel: UILabel = {
-        let label = UILabel()
-        label.font = UIFont.systemFont(ofSize: 14)
-        label.numberOfLines = 0
-        return label
+    let textView: UITextView = {
+        let textView = UITextView()
+        textView.font = UIFont.systemFont(ofSize: 14)
+        textView.isScrollEnabled = false
+        return textView
+    }()
+    
+    let profileImageView: CustomImageVIew = {
+        let imageView = CustomImageVIew()
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        return imageView
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = .systemBlue
+        addSubview(profileImageView)
+        profileImageView.anchor(top: topAnchor, left: leftAnchor, bottom: nil, right: nil, paddingTop: 8, paddingLeft: 8, paddingBottom: 0, paddingRight: 0, width: 40, height: 40)
+        profileImageView.layer.cornerRadius = 40 / 2
         
-        addSubview(textLabel)
-        textLabel.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 4, paddingLeft: 4, paddingBottom: 4, paddingRight: 4, width: 0, height: 0)
+        addSubview(textView)
+        textView.anchor(top: topAnchor, left: profileImageView.rightAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 4, paddingLeft: 4, paddingBottom: 4, paddingRight: 4, width: 0, height: 0)
     }
     
     required init?(coder: NSCoder) {
